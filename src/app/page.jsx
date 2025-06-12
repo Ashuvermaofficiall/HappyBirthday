b5c6f6472de38e48f18a46fe31c69e2b3a3bc25b
Ashu verma <141033217+Ashuvermaofficiall@users.noreply.github.com>
2025-06-12 23:17:56 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(true)
   const [bubbles, setBubbles] = useState([])
   const [showForYouBtn, setShowForYouBtn] = useState(false)
-  const birthdayDate = new Date("June 12, 2025 11:40:01") // Change this date accordingly
+  const birthdayDate = new Date("June 12, 2025 23:30:01") // Change this date accordingly
   const audioRef = useRef(null)
 
   // For testing
