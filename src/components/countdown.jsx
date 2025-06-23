b2b7201279bc8dd0c1a3d321a36eb5ebd3870f4c
Ashu verma <141033217+Ashuvermaofficiall@users.noreply.github.com>
2025-06-23 22:59:42 +0530

    Update countdown.jsx

--- src/components/countdown.jsx
+++ src/components/countdown.jsx
@@ -56,7 +56,7 @@ export default function Countdown({ targetDate, onCountdownEnd }) {
           ease: "easeInOut",
         }}
       >
-        Your Special Day is Almost Here💕
+        Your Special Day is Almost Here 💕
       </motion.h1>
 
       <div className="flex flex-wrap justify-center gap-4 mb-8">
@@ -87,7 +87,7 @@ export default function Countdown({ targetDate, onCountdownEnd }) {
         transition={{ delay: 0.5 }}
       >
         <p className="text-lg text-purple-700 mb-4">
-          Just a little more... A small gift for my favorite person❤️
+          Just a little more... There is a special gift; For the spcial person ❤️
         </p>
 
         <div className="flex justify-center space-x-2">
